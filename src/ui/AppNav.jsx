363dd7a653adac49569fce558108eb676a3815c0
gyhaan <g.yhaan@alustudent.com>
2024-07-03 09:32:09 +0200

    fix bug

--- src/ui/AppNav.jsx
+++ src/ui/AppNav.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function AppNav() {
   return (
@@ -10,13 +11,13 @@ export default function AppNav() {
         <nav>
           <ul>
             <li>
-              <a href="/">Home</a>
+              <Link to="/">Home</Link>
             </li>
             <li>
-              <a href="/products">Products</a>
+              <Link to="/products">Products</Link>
             </li>
             <li>
-              <a href="/checkout">Checkout</a>
+              <Link to="/checkout">Checkout</Link>
             </li>
           </ul>
         </nav>
